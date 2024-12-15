043aafbf11f5ba2c01d757c6d07cdacb2c28f98c
pranitdevops <pnvpranit83@gmail.com>
2024-12-15 11:31:34 +0530

    update

--- 05.array.sh
+++ 05.array.sh
@@ -4,9 +4,9 @@ FRUITS=("banana" "apple" "sithafal")
 
 echo "first fruit is : ${FRUITS[0]}"
 
-echo "first fruit is : ${FRUITS[1]}"
+echo "2nd  fruit is : ${FRUITS[1]}"
 
-echo "first fruit is : ${FRUITS[2]}"
+echo "3rd  fruit is : ${FRUITS[2]}"
 
-echo "first fruit is : ${FRUITS[@]}"
+echo "taotal  fruits are  : ${FRUITS[@]}"
 
