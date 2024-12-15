29a5250694fad2eff34f0a1a9b2016937d12ecdc
pranitdevops <pnvpranit83@gmail.com>
2024-12-15 11:29:32 +0530

    update

---
+++ 05.array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FRUITS=("banana" "apple" "sithafal")
+
+echo "first fruit is : ${FRUITS[0]}"
+
+echo "first fruit is : ${FRUITS[1]}"
+
+echo "first fruit is : ${FRUITS[2]}"
+
+echo "first fruit is : ${FRUITS[@]}"
+
