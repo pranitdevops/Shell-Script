6bc2cecec3c71538925860310a2d0ffa274a8cd3
pranitdevops <pnvpranit83@gmail.com>
2024-12-15 19:36:27 +0530

    update

---
+++ shell/02.intstallsql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ID=$(id -u)#root user decleration
+
+if [ $ID -ne 0 ]
+then
+echo "error : you are not a root user"
+exit 1
+else 
+echo "you are a root user"
+fi
+
+yum install mysql -you
+
